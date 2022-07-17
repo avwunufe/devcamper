d3cf977f36ce7e4261320872dd8312020486ea5a
Frank bellucio <frankbellucio001@gmail.com>
2022-07-17 17:31:36 +0100

    set up bootcamp with optimal router and controller structure

---
+++ routers/bootcamps/bootcamps.controller.js
@@ -0,0 +1,23 @@
+function httpsGetAllBootcamps(req, res, next) {
+    
+}
+function httpsGetOneBootcamp(req, res, next) {
+    
+}
+function createBootcamp(req, res, next) {
+    
+}
+function updateBootcamp(req, res, next) {
+    
+}
+function deleteBootcamp(req, res, next) {
+    
+}
+
+module.exports = {
+    httpsGetAllBootcamps,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp,
+    httpsGetOneBootcamp
+}
--- routers/bootcamps/bootcamps.router.js
+++ routers/bootcamps/bootcamps.router.js
@@ -1,3 +1,14 @@
 const router = require("express").Router()
+const {
+    httpsGetAllBootcamps,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp,
+    httpsGetOneBootcamp
+} = require("./bootcamps.controller")
+
+router.route("/").get(httpsGetAllBootcamps).post(createBootcamp)
+
+router.route("/:id").get(httpsGetOneBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
 module.exports = router
